879aa102233a7cafc87ea31ab9bc345c29f0440d
lsc <dltmd@1502@naver.com>
2024-01-20 17:40:23 +0900

    :white_check_mark: 파티애니멀즈 로컬스토리지 저장기능 추가

--- toyproject/partyAnimals/js/app.js
+++ toyproject/partyAnimals/js/app.js
@@ -1,11 +1,15 @@
 const $addBtn = document.querySelector(".addBtn");
 const $addClose = document.querySelector(".addClose");
 const $addPopup = document.querySelector(".list-add");
+const $todoInput = document.querySelector(".todo-input");
+
 
 $addBtn.addEventListener("click", () => {
     $addPopup.classList.add("on");
+    $todoInput.focus();
 })
 
 $addClose.addEventListener("click", () => {
     $addPopup.classList.remove("on");
+    $todoInput.value = '';
 })
--- toyproject/partyAnimals/js/todo.js
+++ toyproject/partyAnimals/js/todo.js
@@ -1,29 +1,64 @@
 const $todoInputBtn = document.querySelector(".todo-input-btn");
 const $todolistUl = document.querySelector(".todolist-ul");
-const $todoInput = document.querySelector(".todo-input");
+let listData = JSON.parse(localStorage.getItem('listData')) ?? [];
+
+//로컬스토리지에 값이 있으면 리스트 초기화
+resetList();
 
 $todoInputBtn.addEventListener("click", () => {
-    $todolistUl.innerHTML += createTodo($todoInput.value);
+    const listId = Date.now();
+    const listText = $todoInput.value;
+
+    setList(listText, listId);
+    $todolistUl.innerHTML += createTodo(listText, listId);
+
     $todoInput.value = "";
     $addPopup.classList.remove("on");
 })
 
-$todolistUl.addEventListener("click",(e)=> {
+$todolistUl.addEventListener("click", (e) => {
     // 리스트 삭제 버튼 클릭시 li삭제
     const liDeleteBtn = e.target.parentNode;
-    if(liDeleteBtn.classList.contains('todolist-delete')){
+    if (liDeleteBtn.classList.contains('todolist-delete')) {
+        deleteList(liDeleteBtn.parentNode.id);
         liDeleteBtn.parentNode.remove();
     }
 })
 
 
-function createTodo(text) {
+function createTodo(text, id) {
     return (
-        `<li class="todolist-li">
+        `<li class="todolist-li" id=${id}>
             <p>${text}</p>
             <button class="todolist-delete">
                 <i class="fa-solid fa-delete-left"></i>
             </button>
         </li>`
     )
+}
+
+function resetList() {
+    if (listData.length > 0) {
+        $todolistUl.innerHTML = '';
+        listData.forEach(el => {
+            $todolistUl.innerHTML += createTodo(el.text, el.id);
+        });
+    }
+}
+
+function setList(text ,id) {
+    const listDataObj = {
+        text: text,
+        id: id,
+        do: false,
+    }
+    listData.push(listDataObj);
+    localStorage.setItem('listData', JSON.stringify(listData))
+}
+
+function deleteList(id) {
+    listData = listData.filter(el=> {
+        return el.id != id;
+    })
+    localStorage.setItem('listData', JSON.stringify(listData));
 }
