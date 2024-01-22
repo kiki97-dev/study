5044dfdb7455a6f56faa42844dce679e1373c87d
lsc <dltmd@1502@naver.com>
2024-01-22 22:29:51 +0900

    :white_check_mark: 파티애니멀즈 알림창, 캐릭터선택 추가

--- toyproject/partyAnimals/js/app.js
+++ toyproject/partyAnimals/js/app.js
@@ -12,3 +12,25 @@ $addClose.addEventListener("click", () => {
     $addPopup.classList.remove("on");
     $todoInput.value = '';
 })
+
+
+//icon 클릭 이벤트
+const $iconBtn = document.querySelectorAll(".icon>li>button");
+const $main = document.querySelector(".main");
+
+for (let i = 0; i < $iconBtn.length; i++) {
+    let img = new Image();
+    img.src = `./img/desktop-0${i}.png
+    `
+    $iconBtn[i].addEventListener("click", () => {
+        $main.style.backgroundImage = `url(./img/desktop-0${i}.png)`
+    })
+}
+
+//알림창
+const $bell = document.querySelector(".bell");
+const $bellBtn = document.querySelector(".bell-btn");
+
+$bellBtn.addEventListener("click", ()=> {
+    $bell.style.display = 'none';
+})
